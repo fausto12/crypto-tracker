2cfb46fce46d69ea4a41c296d6082d395fdf528b
fausto08 <faustol99@gmail.com>
2022-01-09 02:16:44 -0500

    customized the api call to get multiple data sets

--- cryptosite/crypto/views.py
+++ cryptosite/crypto/views.py
@@ -4,7 +4,14 @@ from django.shortcuts import render
 def home(request):
     import requests
     import json
+
+    # grab crypto price data
+    price_request = requests.get(
+        "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,ADA,DOT,MANA,SHIB,LINK,SOL,XTZ,CRO&tsyms=USD")
+    price = json.loads(price_request.content)
+
+    # grab crypto news
     api_request = requests.get(
         "https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api = json.loads(api_request.content)
-    return render(request, 'home.html', {'api': api})
+    return render(request, 'home.html', {'api': api, 'price': price})
